28b947429254f55cabe3ba411fca2980b151d146
YuDanii <danielsebasgutierrezcastillo@gmail.com>
2019-09-02 21:28:23 -0500

    Clase 2 - vectores

---
+++ Vectores/Assets/Cube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    
+    Vector3 velocity = new Vector3(-2,3,0);
+    Vector3 aceleracion = new Vector3(0,-9.8f,0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+        velocity += aceleracion * Time.deltaTime;
+
+        if(transform.position.y < -5){
+            Debug.Log("Llego al piso");
+            velocity.y *= -1;
+
+        }
+        if(transform.position.x < -10){
+            Debug.Log("Llego al muro");
+            velocity.x *= -1;
+
+        }
+        if(transform.position.x > 10){
+            Debug.Log("Llego al muro");
+            velocity.x *= -1;
+
+        }
+    }
+}
